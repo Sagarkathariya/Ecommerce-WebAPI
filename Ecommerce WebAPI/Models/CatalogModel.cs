fe8af4962b3047c119e042002c3e0342b926d017
Sagarkathariya <sagarkathariya@gmail.com>
2022-11-10 09:09:15 +0545

    wrp

--- Ecommerce WebAPI/Models/CatalogModel.cs
+++ Ecommerce WebAPI/Models/CatalogModel.cs
@@ -5,11 +5,6 @@ namespace Ecommerce_WebAPI.Models
     public class CatalogModel
     {
 
-        public IEnumerable<CatalogItem> CatalogItems { get; set; }
-        public IEnumerable<SelectListItem> Brands { get; set; }
-        public IEnumerable<SelectListItem> Types { get; set; }
-        public int? BrandFilterApplied { get; set; }
-        public int? TypesFilterApplied { get; set; }
-        public PaginationInfo PaginationInfo { get; set; }
+      
     }
 }
